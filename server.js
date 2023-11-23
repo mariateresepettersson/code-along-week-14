366291020c2598fd65510d805dc7b1a1b5f1cfbb
mariateresepettersson <mariateresepettersson>
2023-11-23 11:58:13 +0000

    Practicing mongoose and mongodb

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+
+//Set up a MongoDB connection using Mongoose
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books"
+mongoose.connect(mongoUrl)
+mongoose.Promise = Promise //Mongoose supports different Promise libraries. This line sets Mongoose to use the built-in JavaScript ES6 Promise implementation. This way, Mongoose will use the native Promise globally throughout the application.
+
+//Define the models
+const Author = mongoose.model("Author", {
+  name: String
+})
+
+const Book = mongoose.model("Book", {
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author"
+  }
+})
+
+if (process.env.RESET_DATABASE) {
+  console.log("Resetting database!")
+
+  const seedDatabase = async () => {
+    await Author.deleteMany(); //This code snippet delets any dubblets when saving
+
+    const tolkien = new Author({ name: "J.R.R Tolkien " })
+    await tolkien.save() //Why do we use this save method?
+
+    const rowling = new Author({ name: "J.K. Rowling" })
+    await rowling.save()
+
+    await new Book({ title: "A", author: rowling }).save()
+    await new Book({ title: "B", author: rowling }).save()
+    await new Book({ title: "B", author: rowling }).save()
+  }
+  //invoke the functions
+  seedDatabase();
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -15,6 +54,36 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+app.get("/authors", async (req, res) => {
+  const authors = await Author.find()
+  res.json(authors)
+});
+
+app.get("/authors/:id/", async (req, res) => {
+  const author = await Author.findById(req.params.id)
+  if (author) {
+    res.json(author)
+  } else {
+    res.status(404).json({ error: "Author not found" })
+  }
+})
+
+app.get("/authors/:id/books", async (req, res) => {
+  const author = await Author.findById(req.params.id)
+  if (author) {
+    const books = await Book.find({ author: mongoose.Types.ObjectId(author.id) })
+    res.json(books)
+  } else {
+    res.status(404).json({ error: "Author not found" })
+  }
+})
+
+app.get("/books", async (req, res) => {
+  const books = await Book.find().populate("author")
+  res.json(books)
+})
+
 
 // Start the server
 app.listen(port, () => {
